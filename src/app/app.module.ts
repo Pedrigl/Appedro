51baaba7cd33579c370d4c87db1c3de565515ae6
Pedro <pedrogabrielcruzlima@gmail.com>
2023-03-31 03:26:51 -0300

    tela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClienteComponent } from './cliente/cliente.component';
+import { ClienteService } from './cliente.service';  
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';  
+import { MaterialModule } from './material-module';
+import { MatRadioModule } from '@angular/material/radio';  
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  
 @NgModule({
   declarations: [
     AppComponent,
     ClienteComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,  
+    FormsModule,  
+    ReactiveFormsModule,  
+    HttpClientModule,  
+    BrowserAnimationsModule,  
+    MatRadioModule,  
+    AppRoutingModule  
   ],
-  providers: [],
+  providers: [HttpClientModule, ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
